cc3ebe00d0246c6e5acbced971caaad82fe2a3ae
will <will@MacBook-Pro-cua-will.local>
2023-08-28 21:15:33 +0900

    update

--- cilent/src/index.js
+++ cilent/src/index.js
@@ -4,14 +4,14 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ContextProvider } from "./context/Context";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ContextProvider>
-    <BrowserRouter>
-      <App />
+      <BrowserRouter>
+        <App />
       </BrowserRouter>
     </ContextProvider>
-
   </React.StrictMode>
 );
